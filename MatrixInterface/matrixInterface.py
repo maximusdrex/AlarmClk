3db1f33b9a362b126514e2ad934b7c67260eaae4
BuildTools <unconfigured@null.spigotmc.org>
2017-04-01 18:02:17 -0700

    Full Binary Clock

--- MatrixInterface/matrixInterface.py
+++ MatrixInterface/matrixInterface.py
@@ -7,11 +7,23 @@ spi.xfer([0x0C, 0x01])
 spi.xfer([0x0B, 0xFF])
 
 while(True):
-  for i in range(60):
-    spi.xfer([0x01, 0])
-    spi.xfer([0x01, 0])
-    spi.xfer([0x01, 0])
-    spi.xfer([0x01, i])
-    print(i)
-    time.sleep(1)
+  for i in range(24):
+    spi.xfer([0x03, 0])
+    spi.xfer([0x03, 0])
+    spi.xfer([0x03, 0])
+    spi.xfer([0x03, i])
+    for j in range(60):
+        spi.xfer([0x02, 0])
+        spi.xfer([0x02, 0])
+        spi.xfer([0x02, 0])
+        spi.xfer([0x02, j])
+        print(i)
+        time.sleep(1)
+        for k in range(60):
+            spi.xfer([0x01, 0])
+            spi.xfer([0x01, 0])
+            spi.xfer([0x01, 0])
+            spi.xfer([0x01, k])
+            print("hr: " + str(i) + "; min: " + str(j) + "; sec: " + str(k))
+            time.sleep(1)
 
