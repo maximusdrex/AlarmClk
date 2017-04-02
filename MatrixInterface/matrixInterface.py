f8e07eefedaaf737eb89f0571a0f064fddacee1e
Maxwell <max@theschaefers.com>
2017-04-01 17:58:09 -0700

    Binary Clock

--- MatrixInterface/matrixInterface.py
+++ MatrixInterface/matrixInterface.py
@@ -2,13 +2,16 @@ import spidev
 import time
 spi = spidev.SpiDev()
 spi.open(0, 0)
+spi.lsbfirst = False
+spi.xfer([0x0C, 0x01])
+spi.xfer([0x0B, 0xFF])
 
 while(True):
-    spi.xfer([0x01, 0x02])
-    time.sleep(0.1)
-    spi.xfer([0x01, 0x00])
-    time.sleep(0.1)
-    spi.xfer([0x01, 0x00])
-    time.sleep(0.1)
-    spi.xfer([0x01, 0x00])
-    time.sleep(0.1)
+  for i in range(60):
+    spi.xfer([0x01, 0])
+    spi.xfer([0x01, 0])
+    spi.xfer([0x01, 0])
+    spi.xfer([0x01, i])
+    print(i)
+    time.sleep(1)
+
